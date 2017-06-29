e4f6bda28a10a3bd8088d0c68c3efeaab7e8f976
jingele <tangjiandna@163.com>
2017-06-29 21:16:32 +0800

    2017-06-29

---
+++ DailyNote/solution.cpp
@@ -0,0 +1,5 @@
+sudo update-alternatives --install /usr/bin/java java /usr/lib/jvm/jdk1.8.0_131/bin/java 300
+sudo update-alternatives --install /usr/bin/javac javac /usr/lib/jvm/jdk1.8.0_131/bin/javac 300
+sudo update-alternatives --install /usr/bin/jar jar /usr/lib/jvm/jdk1.8.0_131/bin/jar 300
+sudo update-alternatives --install /usr/bin/javah javah /usr/lib/jvm/jdk1.8.0_131/bin/javah 300
+sudo update-alternatives --install /usr/bin/javap javap /usr/lib/jvm/jdk1.8.0_131/bin/javap 300
